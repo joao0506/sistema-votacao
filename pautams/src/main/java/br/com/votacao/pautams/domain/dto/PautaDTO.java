7d46ae70549b298427642a1c7d80649eaac65bf1
joao0506 <joaoholiveira1@hotmail.com>
2022-12-05 19:42:04 -0300

    Adicionando validação para pautaDTO

--- pautams/src/main/java/br/com/votacao/pautams/domain/dto/PautaDTO.java
+++ pautams/src/main/java/br/com/votacao/pautams/domain/dto/PautaDTO.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class PautaDTO {
     @NotEmpty(message = "Descrição da pauta não pode estar vazia!")
+    @Size(min = 2, max = 80, message = "Descrição da pauta deve conter de 2 a 80 caracteres!")
     private String descricao;
 }
