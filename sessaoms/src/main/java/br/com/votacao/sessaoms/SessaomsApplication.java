b20f34bf4017807562e045fb86146fbea8bbc563
joao0506 <joaoholiveira1@hotmail.com>
2022-12-05 19:55:24 -0300

    Criacao do microservico de sessao

---
+++ sessaoms/src/main/java/br/com/votacao/sessaoms/SessaomsApplication.java
@@ -0,0 +1,13 @@
+package br.com.votacao.sessaoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessaomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessaomsApplication.class, args);
+	}
+
+}
---
+++ sessaoms/src/test/java/br/com/votacao/sessaoms/SessaomsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.votacao.sessaoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessaomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
