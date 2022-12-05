b688898953410171222929aa49b2472edcf8bdca
joao0506 <joaoholiveira1@hotmail.com>
2022-12-05 20:11:29 -0300

    Criando a estrutura da tabela de sessao

---
+++ sessaoms/src/main/java/br/com/votacao/sessaoms/domain/Sessao.java
@@ -0,0 +1,24 @@
+package br.com.votacao.sessaoms.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sessao")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class Sessao {
+    @Id
+    private String id;
+    private LocalDateTime dataAberturaSessao;
+    private LocalDateTime dataFechamentoSessao;
+    @Column(unique = true)
+    private String idPauta;
+}
