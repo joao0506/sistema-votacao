aa06a4d5db989fe4ffdfce7559b5cb2a9c86d746
joao0506 <joaoholiveira1@hotmail.com>
2022-12-05 18:30:37 -0300

    Criacao do microservico de pauta

---
+++ pautams/src/main/java/br/com/votacao/pautams/PautamsApplication.java
@@ -0,0 +1,13 @@
+package br.com.votacao.pautams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PautamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PautamsApplication.class, args);
+	}
+
+}
---
+++ pautams/src/test/java/br/com/votacao/pautams/PautamsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.votacao.pautams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PautamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
